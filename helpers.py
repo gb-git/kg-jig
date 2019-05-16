ca796e3c57aa129c2f0bac85eecbfe8b1d4d1f70
gb-git <gb2017@cafethat.com>
2019-05-16 03:12:34 -0700

    Added functions in helpers to handle loading data files and various initializations. Created new notebook Master-flow-v1.ipynb which will contain the entire workflow that computes what will be submitted. Minor enhancement of text printed when displaying statistics on loaded files.

--- helpers.py
+++ helpers.py
@@ -1,9 +1,12 @@
-# this will import tqdm and enable progress bar for pandas operations
-# source: https://stackoverflow.com/questions/18603270/progress-indicator-during-pandas-operations-python
-from tqdm import tqdm
-tqdm.pandas()
+# --------------------------------------------------------------------------------------
+# Timing
+# --------------------------------------------------------------------------------------
 
+# Operating system functions
+import os
+# Time measurement and processing time
 import time
+
 def start_timer():
     return time.time()
     
@@ -19,3 +22,52 @@ def elapsed_time_ext(msg, elapsed, item_desc, item_count):
     # processing time is measured in ms per item (more useful)
     proc_per_item = elapsed * 1000.0 / item_count
     return msg + ": " + '{0:.3f}'.format(elapsed*1000.0) + " ms for " + str(item_count) + " " + item_desc + "s (" + '{0:.3f}'.format(throughput) + " " + item_desc + "/s, " + '{0:.3f}'.format(proc_per_item) + " ms/" + item_desc + ")"
+
+# --------------------------------------------------------------------------------------
+# Import Needed Resources
+# --------------------------------------------------------------------------------------
+
+print ("Import libraries...")
+timer = start_timer()
+# this will import tqdm and enable progress bar for pandas operations
+# source: https://stackoverflow.com/questions/18603270/progress-indicator-during-pandas-operations-python
+# Allows progress bars to be displayed when loading dataframes in Pandas
+from tqdm import tqdm
+tqdm.pandas()
+# Random generators
+import random
+# Data frames and data manipulation
+import pandas as pd
+# Numerical library
+import numpy as np
+# TQDM: progress bar (https://github.com/tqdm/tqdm)
+import tqdm
+# Plotting library
+import matplotlib.pyplot as plt
+# Statistical data visualization
+import seaborn as sns
+# Natural language toolkit
+import nltk
+import spacy
+# Various operations on collections
+import operator
+print (elapsed_time("Loaded libraries", stop_timer(timer)))
+
+# --------------------------------------------------------------------------------------
+# Initialization of random seeds to ensure reproducibility
+# --------------------------------------------------------------------------------------
+
+def set_seed(seed=0):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+
+# --------------------------------------------------------------------------------------	
+# Set of functions to load the data properly
+# --------------------------------------------------------------------------------------
+
+def load_data(file_path, title=""):
+	timer = start_timer()
+	train_df = pd.read_csv(file_path)
+	print(elapsed_time_ext(title + " load time", stop_timer(timer), "record", len(train_df)))
+	return train_df
